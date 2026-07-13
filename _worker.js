b32fe385b59ef46d84e1f59a598592a81d917735
春野奈 <2273701376@qq.com>
2026-07-13 19:55:34 +0800

    Change R2 bucket hostname in fetch method
    Update R2 bucket hostname for requests.

--- _worker.js
+++ _worker.js
@@ -3,9 +3,8 @@ export default {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
       
-      // 1. 这里必须是纯域名，不能带 https://，不能带后缀路径！
-      // 2. 必须是 R2 桶的公共域名 (xxx.r2.dev)，不能是 cloudflarestorage.com
-      url.hostname = '1717131a88149718861403ea3ada74de.r2.dev'; 
+
+      url.hostname = 'pub-eb56c075642a4c229a1ca8eb4b4ecb31.r2.dev'; 
       
       let new_request = new Request(url, request);
       
