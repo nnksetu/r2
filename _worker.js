7ce256f5bedffd1fea9fb13f13ff52a80689f0ad
春野奈 <2273701376@qq.com>
2026-07-13 19:59:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,13 +2,12 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      
-
-      url.hostname = 'pub-eb56c075642a4c229a1ca8eb4b4ecb31.r2.dev'; 
+      // 1. 填入你正确的纯域名（去掉了 https:// 和路径）
+      url.hostname = 'pub-eb56c075642a4c229a1ca8eb4b4ecb31.r2.dev';
       
       let new_request = new Request(url, request);
       
-      // 关键：必须把客户端带过来的 host 头删掉，让 R2 以为是直接访问 xxx.r2.dev
+      // 2. 核心关键：必须删除 host 头！防止触发 Cloudflare 1000 环路死锁
       new_request.headers.delete("host");
       
       return fetch(new_request);
