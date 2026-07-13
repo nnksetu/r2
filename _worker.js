994e5b79acff67a8cf6597e233b619f73bc2afe5
春野奈 <2273701376@qq.com>
2026-07-13 19:25:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'example.r2.dev'//替换为你的存储桶给出的域名
+      url.hostname = 'https://pub-eb56c075642a4c229a1ca8eb4b4ecb31.r2.dev'//替换为你的存储桶给出的域名
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
