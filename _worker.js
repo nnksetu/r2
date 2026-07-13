da77c8fd530c8a22cce50083163882b0fb537887
春野奈 <2273701376@qq.com>
2026-07-13 19:36:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,9 +2,20 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://pub-eb56c075642a4c229a1ca8eb4b4ecb31.r2.dev'//替换为你的存储桶给出的域名
-      let new_request = new Request(url, request);
-      return fetch(new_request);
+      url.hostname = 'https://1717131a88149718861403ea3ada74de.r2.cloudflarestorage.com/setutime'; // 替换为你的R2域名
+      
+      // 发起请求
+      let response = await fetch(url, {
+        method: request.method,
+        headers: request.headers
+      });
+
+      // 使用流式传输返回，这样就不会把整个大文件一次性吃进 Worker 的内存里
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers
+      });
     }
     return env.ASSETS.fetch(request);
   },
